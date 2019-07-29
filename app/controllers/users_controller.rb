1b5d5816c595c4c7240364909bb48cf463dc4156
Trevahr Robertson <Trevahr@gmail.com>
2019-07-29 16:18:16 -0400

    skeleton version with views done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-
     end
 
     def edit
+        
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+ 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'sessions/logout'
+  post 'logout' => 'sessions#destroy'
   resources :follows
   resources :searches
   resources :favorites
   resources :search_terms
   resources :articles
+  root 'users#homepage'
+  get 'users/:id', to: 'users#show', as: 'user'
   resources :users
-  # root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
