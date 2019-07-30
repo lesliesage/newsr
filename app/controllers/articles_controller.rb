50a19ff2060b16d4d3ef67704793744b8a54650a
Trevahr Robertson <Trevahr@gmail.com>
2019-07-30 10:48:35 -0400

    updated password

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,8 @@
 class ArticlesController < ApplicationController
 
+
+
+    def show
+        @article = Article.find(params[:id])
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
 
     def create
       @user = User.find_by(username: params[:username])
-      if @user
+      if @user && @user.authenticate(params[:passwordç])
           session[:user_id] = @user.id
           redirect_to '/'
       else
+        flash[:notice] = "invalid credentials" #use in an error div
           render :new
       end
     end
